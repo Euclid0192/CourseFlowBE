7a59d50a7f0bb053c927b2649e47ec687ae154db
Nam Nguyen <nguy1053@msu.edu>
2024-01-25 20:09:02 -0500

    Initialize backend for coursesflow frontend

---
+++ src/controllers/flowsController.js
@@ -0,0 +1,20 @@
+
+
+// @desc Get all flows
+// @route GET /flows
+// @access Private
+const getAllFlows = async (req, res) => {
+    console.log('In get all flows...')   
+}
+
+// @desc Save current flow
+// @route POST /flows
+// @access Private
+const saveFlow = async (req, res) => {
+    console.log('In save flow...')
+}
+
+module.exports = {
+    getAllFlows,
+    saveFlow
+}
